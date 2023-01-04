4f01195c67b2b2879a9a76959c70e064b522e97b
Biruk1219 <buruk1219@gmail.com>
2023-01-04 06:00:52 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _puts_recursion - prints string with new line
+ * Return: no return
+ * @s: string that would be print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+	_putchar(*s);
+	_puts_recursion(*s + 1);
+	}
+	else
+		_putchar('\n');
+}
